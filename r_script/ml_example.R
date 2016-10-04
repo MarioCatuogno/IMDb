1cb65db4d13d7ea2b10577512a00c80fc027de98
Mario Catuogno <catuogno.mario88@gmail.com>
2016-10-04 11:17:41 +0200

    Update .R and .Rmd files
    Updated the .R script and .Rmd markdown with Data Visualization code.

--- r_script/ml_example.R
+++ r_script/ml_example.R
@@ -58,15 +58,48 @@ str(data)
 
 ### DATA VISUALIZATION
 
-# Create a data frame with frequencies for each movie genre
-movie_genre_freq <- data %>% 
-  group_by(movie_genre) %>% 
-  summarise(count = n()) %>%
-  arrange(desc(count))
+# Create data frame with frequencies for each movie genre
+movie_genre <- c("Action", "Adventure", "Crime", "Drama", "Comedy", "Family",   
+                 "Fantasy", "History", "Horror", "Musical", "Mystery", "Romance", 
+                 "SciFi", "Sport", "Thriller", "War", "Western")
+movie_freq <- c(2, 11, 11, 46, 22, 20, 
+                53, 1, 4, 4, 15, 53, 
+                76, 10, 151, 19, 9)
+movie_genre_freq <- data.frame(movie_genre, movie_freq)
+rm(movie_genre, movie_freq)
 
-## Number of movies by genre
-p1 <- ggplot(data = movie_genre_freq, aes(x = movie_genre, y = count)) + geom_bar(stat = "identity") + coord_flip()
+# Arrange levels of factor by frequency
+movie_genre_freq$movie_genre <- factor(movie_genre_freq$movie_genre, levels = movie_genre_freq$movie_genre[order(movie_genre_freq$movie_freq)])
+
+## P1: Number of movies by genre
+p1 <- ggplot(data = movie_genre_freq, aes(x = movie_genre, y = movie_freq, label = movie_freq, fill = movie_genre)) + 
+  geom_bar(stat = "identity") + geom_text(nudge_y = 4.5) + coord_flip() +
+  labs(x = "Count", y = "Movie genre", title = "Number of movies by genre") +
+  theme(plot.title = element_text(face = "bold", size = 18),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 10),
+        axis.title = element_text(size = 12),
+        axis.ticks = element_blank(),
+        legend.position = "none", 
+        legend.text = element_text(face = "italic", size = 10),
+        legend.title = element_blank())
 plot(p1)
+
+# P2: Show the correlation between IMDb ratings and personal ratings
+p2 <- ggplot(data = data, aes(x = rating_IMDb, y = rating_personal, colour = rating_personal)) + geom_jitter() +
+  scale_y_continuous(limits = c(1,10), breaks = c(1,3,5,7,9)) +
+  scale_x_continuous(limits = c(1,10), breaks = c(1,3,5,7,9)) +
+  scale_color_continuous(low = "#D73027", high = "#1A9850") +
+  labs(x = "IMDb ratings", y = "Personal ratings", title = "Correlation between \n IMDb and personal ratings") +
+  theme(plot.title = element_text(face = "bold", size = 18),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10),
+        axis.title = element_text(size = 12),
+        axis.ticks = element_blank(),
+        legend.position = "none", 
+        legend.text = element_text(face = "italic", size = 10),
+        legend.title = element_blank())
+plot(p2)
 
 
 
